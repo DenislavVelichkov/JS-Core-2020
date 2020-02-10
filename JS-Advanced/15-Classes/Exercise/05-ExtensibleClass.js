a990be504db9e55d71dfb05dffe350746de7de47
DenislavVelichkov <denislav.velichkov@gmail.com>
2020-02-10 11:34:35 +0200

    Initial commit

--- JS-Advanced/15-Classes/Exercise/05-ExtensibleClass.js
+++ JS-Advanced/15-Classes/Exercise/05-ExtensibleClass.js
@@ -16,3 +16,11 @@
         }
     }
 })();
+
+let obj1 = new Extensible();
+let obj2 = new Extensible();
+let obj3 = new Extensible();
+console.log(obj1.id);
+console.log(obj2.id);
+console.log(obj3.id);
+
