715783a2741ee6b1ef78a27aa5de90e0510f7b14
Denislav Velichkov <denislav.velichkov@gmail.com>
2020-02-16 17:39:31 +0000

    Initial commit.

--- JS-Advanced/19-Exam-Prep-1/01-Ski-Inventory/solution.js
+++ JS-Advanced/19-Exam-Prep-1/01-Ski-Inventory/solution.js
@@ -1,3 +1,21 @@
 function solve() {
-   //ToDO...
+   let addElementButton = document.querySelector("#add-new button");
+
+
+addElementButton.addEventListener('onClick', function (e) {
+   e.preventDefault();
+
+   console.log(fuctionality.addProduct)
+});
+
+}
+
+  let fuctionality = {
+      addProduct: addProductFunc,
+  }
+
+  function addProductFunc(name, quantity, price) {
+
+  }
+
 }
