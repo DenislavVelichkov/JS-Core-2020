4bb819e9dd2327b07a79c009e9e80ec7b1453af6
Denislav Velichkov <denislav.velichkov@gmail.com>
2020-02-16 21:02:48 +0000

    Initial commit.

--- JS-Advanced/19-Exam-Prep-1/01-Ski-Inventory/solution.js
+++ JS-Advanced/19-Exam-Prep-1/01-Ski-Inventory/solution.js
@@ -1,21 +1,49 @@
 function solve() {
-   let addElementButton = document.querySelector("#add-new button");
+   const addElementButton = document.querySelector("#add-new button");
+   const addElement = document.getElementById("add-new");
+   const availableItems = document.querySelector("#products ul");
+   const totalPrice = document.querySelectorAll("h1")[1];
+   const myProducts = document.getElementById("myProducts");
 
+   addElementButton.addEventListener("click", addProductHandler);
 
-addElementButton.addEventListener('onClick', function (e) {
-   e.preventDefault();
+   function addProductHandler(ev) {
+      ev.preventDefault();
 
-   console.log(fuctionality.addProduct)
-});
+      let name = addElement[0].value;
+      let quantity = addElement[1].value;
+      let price = addElement[2].value;
 
-}
+      let newElement = document.createElement("li");
+      newElement.innerHTML = `<span>${name}</span>\n
+                              <strong>Available: ${quantity}</strong>\n
+                              <div>\n
+                              <strong>${price}</strong>\n
+                              <button>Add to Client's List</button>\n
+                              </div>\n`;
 
-  let fuctionality = {
-      addProduct: addProductFunc,
-  }
+      newElement.addEventListener("click", addProductToMyProducts);
+      availableItems.appendChild(newElement);
+   }
 
-  function addProductFunc(name, quantity, price) {
+   function addProductToMyProducts(ev) {
+      ev.preventDefault()
 
-  }
+      let availableCount = parseInt(this.querySelector("strong").innerHTML.match(/[0-9]+/g)[0]);
+      let oldPrice = parseFloat(totalPrice.innerHTML.match(/[0-9.]+/g)[0]);
+      let newPrice = parseFloat(this.querySelector("div strong").innerText) + oldPrice;
 
+      if (availableCount - 1 <= 0) {
+         availableItems.removeChild(this);
+      }
+      
+      this.querySelector("strong").innerHTML = `Available: ${availableCount - 1}`
+      totalPrice.innerHTML = `Total Price: ${newPrice}`;
+      let newElement = document.createElement("li");
+      let productName = this.querySelector("span").innerText;
+      let productPrice = this.querySelector("div strong").innerText;
+      newElement.innerHTML = `${productName}\n<strong>${productPrice}</strong>`;
+      myProducts.appendChild(newElement);
+
+   }
 }
