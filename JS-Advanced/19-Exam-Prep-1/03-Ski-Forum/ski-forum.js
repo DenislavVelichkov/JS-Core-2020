fcd043b028bfb8dc0f0a7164fc8fa76d928fd3e0
DenislavVelichkov <denislav.velichkov@gmail.com>
2020-02-17 13:47:09 +0200

    Initial commit.

--- JS-Advanced/19-Exam-Prep-1/03-Ski-Forum/ski-forum.js
+++ JS-Advanced/19-Exam-Prep-1/03-Ski-Forum/ski-forum.js
@@ -16,31 +16,29 @@ class Forum {
       throw new Error("Passwords do not match");
     }
 
-    if (this.users.find(x => x.username === username || x.email === email)) {
+    if (this.users.find(u => u.username === username || u.email === email)) {
       throw new Error("This user already exists!");
     }
 
-    let user = {
+    this.users.push({
       username: username,
       password: password,
       email: email
-    }
-
-    this.users.push(user);
+    });
 
     return `${username} with ${email} was registered successfully!`
-  };
+  }
 
   login(username, password) {
 
-    if (this.users.find(x => x.username !== username)) {
+    if (!this.users.find(u => u.username === username)) {
       throw new Error("There is no such user");
     }
 
-    let user = this.users.find(x => x.username === username && x.password === password);
+    let userToLogin = this.users.find(u => u.username === username && u.password === password);
 
-    if (user) {
-      this.loggedInUsers.push(user);
+    if (userToLogin) {
+      this.loggedInUsers.push(userToLogin);
 
       return "Hello! You have logged in successfully";
     }
@@ -49,34 +47,94 @@ class Forum {
 
   logout(username, password) {
 
-    if (this.users.find(x => x.username !== username)) {
+    if (!this.users.find(x => x.username === username)) {
       throw new Error("There is no such user");
     }
 
     let user = this.users.find(x => x.username === username && x.password === password);
 
     if (user) {
-      this.loggedInUsers.filter(u => u.username !== user.username);
-      console.log(this.loggedInUsers)
-      return "You have logged out successfully Hello! You have logged in successfully";
-    }
+      this.loggedInUsers = this.loggedInUsers.reduce((acc, current) => {
+        if (current.username !== user.username) {
+          acc.push(current)
+        }
 
+        return acc;
+      }, []);
+
+      return "You have logged out successfully!";
+    }
   }
 
   postQuestion(username, question) {
-    let question = {
-      id: this.id
-    };
 
-    // if () {
+    if (!this.loggedInUsers.find(u => u.username === username)) {
+      throw new Error("You should be logged in to post questions");
 
-    // }
+    } else if (!question) {
 
-    this.questions.push(question);
+      throw new Error("Invalid question");
+    } else {
+      this.questions.push({
+        id: this.id++,
+        username: username,
+        question: question,
+        answers: [],
+      });
+
+      return "Your question has been posted successfully";
+    }
+  }
+
+  postAnswer(username, questionId, answer) {
+    const question = this.questions.find(q => q.id === questionId);
+    const isUserLoggedIn = this.loggedInUsers.find(u => u.username === username);
+    const doesUserExist = this.users.find(u => u.username === username);
+
+    if (!(isUserLoggedIn && doesUserExist)) {
+      throw new Error("You should be logged in to post answers")
+    }
+
+    if (!answer) {
+      throw new Error("Invalid answer")
+    }
+
+    if (!question) {
+      throw new Error("There is no such question")
+    }
+
+    question.answers.push({
+      username: username,
+      answer: answer
+    })
+
+    return "Your answer has been posted successfully";
+  }
+
+  showQuestions() {
+    let result = "";
+    this.questions.forEach(q => {
+      result += `Question ${q.id} by ${q.username}: ${q.question}\n`
+      q.answers.forEach(a => {
+        result += `---${a.username}: ${a.answer}\n`
+      });
+    });
+
+    return result.trim();
   }
 }
 
 let forum = new Forum();
-console.log(forum.register("Alabala", "Asd123", "Asd123", "aa@aa.aa"));
-console.log(forum.login("Alabala", "Asd123"));
-console.log(forum.logout("Alabala", "Asd123"));
+
+forum.register('Michael', '123', '123', 'michael@abv.bg');
+forum.register('Stoyan', '123ab7', '123ab7', 'some@michael@.com');
+forum.login('Michael', '123');
+forum.login('Stoyan', '123ab7');
+
+forum.postQuestion('Michael', "Alabala");
+// forum.postAnswer('Stoyan', 1, "Yes, I have rented one last year.");
+forum.postQuestion('Stoyan', "How long are supposed to be the ski for my daughter?");
+forum.postAnswer('Michael', 2, "How old is she?");
+forum.postAnswer('Michael', 2, "Tell us how tall she is.");
+
+console.log(forum.showQuestions());
