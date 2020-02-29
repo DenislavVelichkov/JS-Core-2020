97e59e4d6280b9423599ece3b024afe58df21306
Denislav Velichkov <denislav.velichkov@gmail.com>
2020-02-29 13:17:54 +0200

    Initial commit

---
+++ JS-Applications/5-Object-Composition/1-Array-Extension.js
@@ -0,0 +1,26 @@
+(function solve () {
+
+  Array.prototype.last = function () {
+    return this[this.lenght - 1]
+  }
+
+  Array.prototype.skip = function (n) {
+    let newArr = []
+    for (let index = n; index < this.length; index++) {
+      newArr.push(this[index])
+    }
+
+    return newArr
+  }
+
+  Array.prototype.take = function (n) {
+
+    return this[this.lenght - 1]
+  }
+
+  Array.prototype.sum = () => {
+    return this[this.lenght - 1]
+  }
+
+  console.log(b)
+}())
