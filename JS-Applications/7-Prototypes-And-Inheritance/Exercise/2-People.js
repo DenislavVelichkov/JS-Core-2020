805cbdc9d22f885dbd38d52f66e9f7b51d7fca52
Denislav Velichkov <denislav.velichkov@gmail.com>
2020-03-07 12:05:52 +0200

    Update

--- JS-Applications/7-Prototypes-And-Inheritance/Exercise/2-People.js
+++ JS-Applications/7-Prototypes-And-Inheritance/Exercise/2-People.js
@@ -1,39 +1,39 @@
-function solve () {
+(() => {
   class Employee {
-    constructor (name, age) {
+    constructor(name, age) {
+
       if (new.target === Employee) {
         throw new Error('Cannot instantiate directly')
       }
 
       this.name = name
       this.age = Number(age)
-      this.salary = Number(0)
+      this.salary = 0
       this.tasks = []
     }
 
-    work () {
+    work() {
       let currentTask = this.tasks.shift()
       console.log(`${this.name} + ${currentTask}`)
       this.tasks.push(currentTask)
     }
 
-    collectSalary () {
-      console.log(`${this.name} received ${this.getSalary()} this month`)
+    collectSalary() {
+      console.log(`${this.name} received ${this.salary} this month`)
     }
-
 
   }
 
   class Junior extends Employee {
-    constructor (name, age) {
-      super(name, Number(age))
+    constructor(name, age) {
+      super(name, age)
       this.tasks = [` is working on a simple task.`]
     }
   }
 
   class Senior extends Employee {
-    constructor (name, age) {
-      super(name, Number(age))
+    constructor(name, age) {
+      super(name, age)
       this.tasks = [
         ` is working on a complicated task.`,
         ` is taking time off work.`,
@@ -43,14 +43,14 @@ function solve () {
   }
 
   class Manager extends Employee {
-    constructor (name, age) {
-      super(name, Number(age))
+    constructor(name, age) {
+      super(name, age)
       this.dividend = 0
       this.tasks = [` scheduled a meeting.`, ` is preping a quarterly report.`]
     }
 
-    getSalary () {
-      return this.salary + this.dividend
+    collectSalary() {
+      console.log(`${this.name} received ${this.salary + this.dividend} this month`)
     }
   }
 
@@ -60,4 +60,5 @@ function solve () {
     Senior,
     Manager
   }
-}
+
+})()
