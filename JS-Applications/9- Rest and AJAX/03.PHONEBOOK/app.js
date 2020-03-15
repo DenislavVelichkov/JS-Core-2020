0dba36fb97f16554a8751ea0202aed1f6d51fef4
DenislavVelichkov <denislav.velichkov@gmail.com>
2020-03-15 23:03:32 +0200

    Update

--- JS-Applications/9- Rest and AJAX/03.PHONEBOOK/app.js
+++ JS-Applications/9- Rest and AJAX/03.PHONEBOOK/app.js
@@ -1,74 +1,80 @@
 function attachEvents() {
-    const url = "https://phonebook-nakov.firebaseio.com/phonebook.json";
-    const phoneBookData = document.getElementById("phonebook");
+    const url = "https://phone-book-fe9f7.firebaseio.com/phonebook.json"
+    const phoneBookData = document.getElementById("phonebook")
 
     function createPhone() {
         const person = document.getElementById("person").value;
         const phone = document.getElementById("phone").value;
+        const recordToAdd = { person, phone }
+
         const headers = {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({
-                person,
-                phone
-            })
+            body: JSON.stringify(recordToAdd)
         };
+
         fetch(url, headers)
-            .then(() => {
+            .then((response) => response.json())
+            .then(data => {
                 document.getElementById("person").value = "";
                 document.getElementById("phone").value = "";
-                phoneBookData.innerHTML = "";
-                loadPhoneBook();
+
+                createDbRecord(recordToAdd, data.name);
             })
-            .catch(() => console.log("Error"));
+            .catch((e) => console.log(e));
     }
 
     function loadPhoneBook() {
         fetch(url)
-            .then(resources => resources.json())
+            .then(r => r.json())
             .then(data => {
                 phoneBookData.innerHTML = "";
 
-                Object.entries(data).forEach(([elementId, phoneBook]) => {
-                    const {
-                        person,
-                        phone
-                    } = phoneBook;
+                Object.entries(data).forEach((record) => {
+                    const [recordId, personInfo] = record
 
-                    const li = document.createElement("li");
-                    li.textContent = `${person}: ${phone}`;
-                    const deleteButton = document.createElement("button");
-                    deleteButton.textContent = "Delete";
-                    deleteButton.id = elementId;
-                    deleteButton.addEventListener("click", deletePhone);
-                    li.appendChild(deleteButton);
-                    phoneBookData.appendChild(li);
-                });
+                    if (recordId && personInfo) {
+
+                        createDbRecord(personInfo, recordId);
+                    }
+
+                })
             })
-            .catch(() => console.log("Error"));
+            .catch((e) => console.log(e))
     }
+
 
     function deletePhone() {
-        const id = this.getAttribute("id");
-        const headers = {
-            method: "DELETE"
-        };
+        const recordToRemove = this.parentNode;
+        const deleteQuery =
+            `https://phone-book-fe9f7.firebaseio.com/phonebook/${recordToRemove.id}.json`
 
-        fetch(`https://phonebook-nakov.firebaseio.com/phonebook/${id}.json`, headers)
+        fetch(deleteQuery, {method: "DELETE"})
             .then(() => {
-                phoneBookData.innerHTML = "";
-                loadPhoneBook();
+                recordToRemove.remove()
             })
             .catch(() => console.log("Error"));
+
     }
 
-    return {
-        loadPhoneBook,
-        createPhone,
-        deletePhone
-    };
+    function createDbRecord(personInfo, recordId) {
+        const li = document.createElement("li");
+        li.textContent = `${personInfo.person}: ${personInfo.phone}`;
+        li.id = recordId;
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", deletePhone);
+        li.appendChild(deleteButton);
+        phoneBookData.appendChild(li);
+    }
+
+    const createBtn = document.querySelector("#btnCreate")
+    createBtn.addEventListener('click', createPhone)
+
+    const loadBtn = document.querySelector("#btnLoad")
+    loadBtn.addEventListener('click', loadPhoneBook)
 }
 
 attachEvents();
